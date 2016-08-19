85b5a906e5f236082c17ec18016ae3b2bacff7d2
tavo <tavo198718@gmail.com>
2016-08-18 23:35:30 -0500

    Add new helpers

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,6 +1,7 @@
 <?php
 
-function view($template, $vars = []){
+function view($template, $vars = [])
+{
     $loader = new Twig_Loader_Filesystem('resources/views/');
     $twig = new Twig_Environment($loader);
     echo $twig->render($template, $vars);
@@ -9,23 +10,17 @@ function view($template, $vars = []){
 
 function controller($controller)
 {
-    $controller = $controller.'Controller';
+    $controller = ucfirst($controller).'Controller';
 
     $file = __DIR__. "/../app/Controllers/$controller".".php";
 
-    if (file_exists($file)){
-
-       require $file;
-
-    }else{
-
+    if (file_exists($file)) {
+        require $file;
+    } else {
         http_response_code(404);
 
         return view('404.twig');
     }
-
-    //require __DIR__. "/../app/Controllers/$controller"."Controller.php";
-
 }
 
 
@@ -33,17 +28,18 @@ function controller($controller)
  * @param $data
  * @return mixed|string
  */
-function clean_input($data) {
+function clean_input($data)
+{
     $data = trim($data);
     $data = strip_tags($data);
     $data = str_replace(
-        array("\\", "¨", "º", "-"," ",
+        array("\\", "¨", "º", "-", " ",
             "#", "@", "|", "!", "\"",
             "·", "$", "%", "&", "/",
             "(", ")", "?", "'", "¡",
             "¿", "[", "^", "<code>", "]",
             "+", "}", "{", "¨", "´",
-            ">", "<", ";", ",", ":","Ç"),
+            ">", "<", ";", ",", ":", "Ç"),
         '',
         $data
     );
@@ -51,4 +47,50 @@ function clean_input($data) {
     return $data;
 }
 
+function validateData($post)
+{
+    $errors =false;
 
+    foreach ($post as $key => $value) {
+        if (!isset($key) || empty($value)) {
+            $errors = true;
+        }
+    }
+
+    return $errors;
+}
+
+function isPost()
+{
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        return true;
+    }
+
+    return false;
+}
+
+function cleanRequest($post = array())
+{
+    foreach ($post as $key => $value) {
+        if (!isEmail($key)) {
+            $post[$key] = clean_input($value);
+        }
+    }
+
+    return $post;
+}
+
+function isEmail($key)
+{
+    if ($key == 'email') {
+        return true;
+    }
+
+    return false;
+}
+
+
+function redirect($url)
+{
+    header('Location: '.getenv('SITE_URL').$url);
+}
