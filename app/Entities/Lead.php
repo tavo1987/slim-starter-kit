3f83f697ce8cab9fdadb9c69986d384958897dff
tavo <tavo198718@gmail.com>
2016-08-18 23:40:34 -0500

    Add entity Lead

---
+++ app/Entities/Lead.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Entities;
+
+use Illuminate\Database\Eloquent\Model as Model;
+
+class Lead extends Model
+{
+
+    protected $table = 'leads';
+
+    public $timestamps = false;
+
+    public function getFullNameAttribute($value)
+    {
+        return $this->attributes['name'];
+    }
+}
