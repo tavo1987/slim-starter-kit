c55f1c6a10b561842eeb294c2060f3329f127245
Edwin Ramírez <tavo198718@gmail.com>
2016-09-28 12:15:47 -0500

    Add file for routes

---
+++ app/routes.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Controllers\HomeController;
+use App\Controllers\LeadController;
+use App\Controllers\ErrorController;
+use App\Controllers\ThanksController;
+
+$app->get('/', [HomeController::class, 'index']);
+$app->post('/store', [LeadController::class, 'store']);
+$app->get('/error', [ErrorControllerController::class, 'index']);
+$app->get('/thanks', [ThanksController::class, 'index']);
+
