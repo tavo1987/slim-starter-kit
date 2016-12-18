2ae5c7aa38153178cecfeb75729cbbf306ed435b
tavo <tavo198718@gmail.com>
2016-12-17 23:27:34 -0500

    Update Lead Controller

--- app/Controllers/LeadController.php
+++ app/Controllers/LeadController.php
@@ -5,27 +5,50 @@ namespace App\Controllers;
 use App\Entities\Lead;
 use Core\Controllers\EmailController  as Email;
 
-class LeadController
+class LeadController extends BaseController
 {
+    /**
+     * Validations rules to $_POST data
+     * @var [array]
+     */
+    protected $rules = [
+            'required' => [
+                ['name'],
+                ['email'],
+            ],
+            'lengthMin' => [
+                ['name', 3],
+            ],
+
+            'email' => 'email',
+        ];
+
+    /**
+     * Input labels
+     * @var array
+     */
+    protected $labels = [
+        'name'  => 'Name',
+        'email' => 'Email',
+    ];
+
 
     public function store()
     {
-        $post   = cleanRequest($_POST);
-        $errors = validateData($post);
+        $errors = $this->validate($_POST, $this->rules, $this->labels);
 
         if ($errors) {
-            redirect('error');
-            exit();
+            return view('home.twig', compact('errors'));
         }
 
-        $request = (object) $post;
+        $request = (object) cleanRequest($_POST);
 
         $lead            = new Lead();
         $lead->name      = $request->name;
         $lead->email     = $request->email;
         $lead->date      = date('Y-m-d H:i:s');
 
-        if (!$this->isRegistered($request->email)) {
+        if (!$lead->isRegistered($request->email)) {
             $lead->save();
         }
 
@@ -33,11 +56,5 @@ class LeadController
         Email::send(getenv('ADMIN_EMAIL'), getenv('ADMIN_EMAIL_SUBJECT'), 'admin', $lead);
 
         redirect('thanks');
-    }
-
-    public function isRegistered($email)
-    {
-        $lead = Lead::where('email', '=', $email)->first();
-        return $lead ? 1 : 0 ;
     }
 }
