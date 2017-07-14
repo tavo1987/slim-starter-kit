54b70bccc9b11a4b6bfee3bffea0a2ad5efd1516
tavo1987 <tavo198718@gmail.com>
2017-07-13 20:53:31 -0500

    Added vuejs, foundation, and jquery configuration

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,10 +1,21 @@
 
+import $ from "jquery";
+import 'foundation-sites';
 
-/*======================================================================
-	ARCHIVOS JS
- =======================================================================*/
+import Vue from 'vue';
+import VeeValidate from 'vee-validate';
 
-//@prepros-prepend partials/jquery.js
-//@prepros-prepend partials/foundation.js
-//@prepros-prepend partials/what-input.js
-//@prepros-append  partials/main.js
+
+$(document).foundation();
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+Vue.use(VeeValidate);
+Vue.component('app-form', require('./components/Form.vue'));
+const app = new Vue({
+   el: '#app'
+});
