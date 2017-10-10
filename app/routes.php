3754fc1664f9cc60220912a7c2de3446d923cd2a
tavo <tavo198718@gmail.com>
2017-10-10 06:23:04 -0500

    Updated routes

--- app/routes.php
+++ app/routes.php
@@ -2,11 +2,13 @@
 
 use App\Controllers\HomeController;
 use App\Controllers\LeadController;
-use App\Controllers\ErrorController;
 use App\Controllers\ThanksController;
 
 $app->get('/', [HomeController::class, 'index']);
 $app->post('/store', [LeadController::class, 'store']);
-$app->get('/error', [ErrorController::class, 'index']);
-$app->get('/thanks', [ThanksController::class, 'index']);
+$app->get('/lead/{name}', [LeadController::class, 'search']);
+$app->get('/error', function (){
+    return view('error.twig');
+});
+$app->get('/thanks/{name}', [ThanksController::class, 'index']);
 
