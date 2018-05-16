1477a14c895bc9ec67b3dc6c42c92a56861ab417
tavo1987 <tavo198718@gmail.com>
2018-05-16 17:26:41 -0500

    Update thanks.twig and LeadController

--- app/Controllers/LeadController.php
+++ app/Controllers/LeadController.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use App\Entities\Lead;
 use Core\Controllers\EmailController  as Email;
+use Symfony\Component\VarDumper\Dumper\DataDumperInterface;
 
 class LeadController extends BaseController
 {
@@ -32,28 +33,24 @@ class LeadController extends BaseController
         'email' => 'Email',
     ];
 
-    public function store()
+    public function store($request, $response)
     {
-        $errors = $this->validate($_POST, $this->rules, $this->labels);
+        //$errors = $this->validate($_POST, $this->rules, $this->labels);
 
-        if ($errors) {
+       /* if ($errors) {
             return view('home.twig', compact('errors'));
-        }
-
-        $request = (object) cleanRequest($_POST);
+        }*/
 
         $lead            = new Lead();
-        $lead->name      = $request->name;
-        $lead->email     = $request->email;
+        $lead->name      = $request->getParam('name');
+        $lead->email     = $request->getParam('email');
         $lead->date      = date('Y-m-d H:i:s');
 
-        if (!$lead->isRegistered($request->email)) {
+        if (!$lead->isRegistered($lead->email)) {
             $lead->save();
         }
-        Email::send($lead->email, getenv('LEAD_EMAIL_SUBJECT'), 'lead', $lead);
-        Email::send(getenv('ADMIN_EMAIL'), getenv('ADMIN_EMAIL_SUBJECT'), 'admin', $lead);
 
-        redirect("thanks/{$lead->name}");
+	    return $response->withRedirect('/thanks', 301);
     }
 
     /**
