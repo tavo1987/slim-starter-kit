74ff4f8e835b2f7d5933cfd3e332cec5acc6e308
tavo1987 <tavo198718@gmail.com>
2018-05-17 13:47:15 -0500

    Remove validate method from base controller

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -3,8 +3,6 @@
 namespace  App\Controllers;
 
 use Interop\Container\ContainerInterface;
-use Slim\Views\Twig;
-use Valitron\Validator as Valitron;
 
 class BaseController
 {
@@ -16,23 +14,4 @@ class BaseController
 		$this->container = $container;
 		$this->view = $this->container['view'];
 	}
-
-	/**
-     * @param $data
-     * @param $rules
-     * @param $labels
-     * @return array|bool
-     */
-    public function validate($data, $rules, $labels)
-    {
-        $valitron = new Valitron($data);
-        $valitron->rules($rules);
-        $valitron->labels($labels);
-
-        if (!$valitron->validate()) {
-            $collection = collection($valitron->errors());
-            return $collection->flatten()->all();
-        }
-        return false;
-    }
 }
