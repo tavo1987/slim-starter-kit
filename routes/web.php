852fbecba49a61a3cdb28fa0c970aa5b0e580634
tavo <tavo198718@gmail.com>
2018-05-16 22:34:43 -0500

    Update web routes

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,7 @@ use App\Controllers\ThanksController;
 
 $app->get('/', HomeController::class.':index');
 
-$app->get('/test/{id}', HomeController::class.':test');
-
 $app->post('/store', LeadController::class.':store');
 
-$app->get('/lead/{name}', LeadController::class.':search');
-
-$app->get('/thanks/{name}', ThanksController::class.':index');
+$app->get('/thanks', ThanksController::class.':index');
 
