780d5b5e0c6e04dbc76edcbc5a144c2916f63352
tavo1987 <tavo198718@gmail.com>
2018-05-17 13:49:11 -0500

    Set names to default routes.

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,7 @@ use App\Controllers\HomeController;
 use App\Controllers\LeadController;
 use App\Controllers\ThanksController;
 
-$app->get('/', HomeController::class.':index');
-
-$app->post('/store', LeadController::class.':store');
-
-$app->get('/thanks', ThanksController::class.':index');
+$app->get('/', HomeController::class.':index')->setName('home');
+$app->post('/store', LeadController::class.':store')->setName('store');
+$app->get('/thanks', ThanksController::class.':index')->setName('thanks');
 
