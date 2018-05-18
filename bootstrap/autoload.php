621a58f47549de302cdc794d5107be657fef8ab1
tavo <tavo198718@gmail.com>
2018-05-17 21:49:22 -0500

    Add new config files to clean autoload.php

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -1,13 +1,8 @@
 <?php
 
 use App\Validation\Validator;
-use Slim\App;
 use Slim\Csrf\Guard;
-use Slim\Http\Uri;
-use Slim\Middleware\Session;
-use Slim\Views\Twig;
 use SlimSession\Helper;
-use Zeuxisoo\Whoops\Provider\Slim\WhoopsMiddleware;
 use Valitron\Validator as V;
 
 /**
@@ -38,94 +33,12 @@ date_default_timezone_set(getenv('SET_TIME_LOCATE'));
 /**
  * App Init
  */
-$app = new App([
-	'settings' => [
-		'displayErrorDetails' => getenv('APP_DEBUG') === 'true',
-		'db' => [
-			'driver' => 'mysql',
-			'host' => getenv('DB_HOST'),
-			'database' => getenv('DB_DATABASE'),
-			'username' => getenv('DB_USERNAME'),
-			'password' => getenv('DB_PASSWORD'),
-			'charset'   => 'utf8',
-			'collation' => 'utf8_unicode_ci',
-			'prefix'    => '',
-		],
-		'debug' => getenv('APP_DEBUG') === 'true',
-		'whoops.editor' => 'sublime',
-		'app' => [
-			'name' => getenv('APP_NAME')
-		],
-		'views' => [
-			'cache' => getenv('VIEW_CACHE_DISABLED') === 'true' ? false : __DIR__ . '/../storage/views'
-		]
-	],
-]);
+require_once __DIR__ . '/../config/app.php';
 
 /**
  * Get container
  */
 $container = $app->getContainer();
-
-/**
- * Auth support
- * @param $container
- *
- * @return \App\Auth\Auth
- */
-$container['auth'] = function ($container) {
-	return new \App\Auth\Auth($container);
-};
-
-/**
- * Flash messages
- *
- * @param $container
- *
- * @return \Slim\Flash\Messages
- */
-$container['flash'] = function ($container) {
-	return new \Slim\Flash\Messages;
-};
-
-/**
- * Twig configuration
- * @param $container
- *
- * @return Twig
- */
-$container['view'] = function ($container) {
-	$view = new Twig(__DIR__ . '/../resources/views', [
-		'cache' => $container->settings['views']['cache']
-	]);
-	// Instantiate and add Slim specific extension
-	$router = $container->get('router');
-	$uri = Uri::createFromEnvironment(new \Slim\Http\Environment($_SERVER));
-	$view->addExtension(new \Slim\Views\TwigExtension($router, $uri));
-
-	$view->getEnvironment()->addGlobal('auth', [
-		'check' => $container->auth->check(),
-		'user' => $container->auth->user(),
-	]);
-
-	$view->getEnvironment()->addGlobal('flash', $container->flash);
-
-	return $view;
-};
-
-/**
- * Eloquent configuration
- */
-require_once __DIR__ . '/../config/database.php';
-
-/**
- * @param $container
- *
- * @return Validator
- */
-$container['validator'] = function ($container) {
-	return new Validator($container);
-};
 
 /**
  * Registering Globally Session Helpers
@@ -144,22 +57,53 @@ $container['csrf'] = function () {
 /**
  * Middleware
  */
-$app->add(new WhoopsMiddleware);
-$app->add(new \App\Middleware\ValidationErrorsMiddleware($container));
-$app->add(new \App\Middleware\OldInputMiddleware($container));
-$app->add(new \App\Middleware\CsrfViewMiddleware($container));
-$app->add($container->csrf);
-$app->add(new Session([
-	'name' => getenv('APP_NAME'),
-	'autorefresh' => true,
-	'lifetime' => '1 hour'
-]));
+require_once __DIR__ . '/../config/middleware.php';
+
+/**
+ * Auth support
+ * @param $container
+ *
+ * @return \App\Auth\Auth
+ */
+$container['auth'] = function ($container) {
+	return new \App\Auth\Auth($container);
+};
+
+/**
+ * Flash messages
+ *
+ * @return \Slim\Flash\Messages
+ */
+$container['flash'] = function () {
+	return new \Slim\Flash\Messages;
+};
+
+
+/**
+ * Eloquent configuration
+ */
+require_once __DIR__ . '/../config/database.php';
+
+/**
+ * Validation
+ *
+ * @param $container
+ *
+ * @return Validator
+ */
+$container['validator'] = function ($container) {
+	return new Validator($container);
+};
+
+/**
+ * Twig configuration
+ * @param $container
+ *
+ * @return Twig
+ */
+require_once __DIR__ . '/../config/view.php';
 
 /*
  * Routes
  */
-require_once __DIR__ . '/../routes/web.php';
-
-
-
-
+require_once __DIR__ . '/../config/routes.php';
--- routes/web.php
+++ routes/web.php
@@ -1,42 +1,9 @@
 <?php
 
-use App\Controllers\Admin\LeadsReportController;
-use App\Controllers\Auth\AuthController;
-use App\Controllers\Auth\PasswordController;
 use App\Controllers\HomeController;
 use App\Controllers\LeadController;
 use App\Controllers\ThanksController;
-use App\Middleware\AuthMiddleware;
-use App\Middleware\GuestMiddleware;
 
-/**
- * Guest Routes
- */
-	$app->get('/', HomeController::class.':index')->setName('home');
-	$app->post('/store', LeadController::class.':store')->setName('store');
-	$app->get('/thanks', ThanksController::class.':index')->setName('thanks');
-
-
-/**
- * Guest users
- */
-
-$app->group('', function () {
-	$this->get('/auth/signup', AuthController::class.':getSignUp')->setName('auth.signup');
-	$this->post('/auth/signup', AuthController::class.':postSignUp');
-
-	$this->get('/auth/signin', AuthController::class.':getSignIn')->setName('auth.signin');
-	$this->post('/auth/signin', AuthController::class.':postSignIn');
-})->add(new GuestMiddleware($container));
-
-/**
- * Auth Users
- */
-$app->group('', function () {
-	$this->get('/reports', LeadsReportController::class.':index')->setName('reports');
-	$this->get('/auth/signout', AuthController::class.':getSignOut')->setName('auth.signout');
-
-	$this->get('/auth/password/change', PasswordController::class.':getChangePassword')->setName('auth.password.change');
-	$this->post('/auth/password/change', PasswordController::class.':postChangePassword');
-})->add(new AuthMiddleware($container));
-
+$app->get('/', HomeController::class.':index')->setName('home');
+$app->post('/store', LeadController::class.':store')->setName('store');
+$app->get('/thanks', ThanksController::class.':index')->setName('thanks');
