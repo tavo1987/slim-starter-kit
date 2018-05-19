34840866dce6c583936f95875a6ed9558823c657
tavo <tavo198718@gmail.com>
2018-05-19 15:07:35 -0500

    Update middleware order on middleware.php

--- config/middleware.php
+++ config/middleware.php
@@ -7,10 +7,10 @@ use Slim\Middleware\Session;
 use Zeuxisoo\Whoops\Provider\Slim\WhoopsMiddleware;
 
 $app->add(new WhoopsMiddleware);
-$app->add(new ValidationErrorsMiddleware($container));
-$app->add(new OldInputMiddleware($container));
 $app->add(new CsrfViewMiddleware($container));
 $app->add($container->csrf);
+$app->add(new ValidationErrorsMiddleware($container));
+$app->add(new OldInputMiddleware($container));
 $app->add(new Session([
 	'name' => getenv('APP_NAME'),
 	'autorefresh' => true,
