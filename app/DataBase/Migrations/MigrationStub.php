2ee71883f658a7f890714d75f97d66999041b2d2
tavo <tavo198718@gmail.com>
2018-05-19 13:42:28 -0500

    Update migration template for phinx

--- app/DataBase/Migrations/MigrationStub.php
+++ app/DataBase/Migrations/MigrationStub.php
@@ -1,16 +1,37 @@
 <?php
 
 use $useClassName;
+use Illuminate\Database\Schema\Blueprint;
 
 class $className extends $baseClassName
 {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
 	public function up()
 	{
-		//
+	    //Create new table
+		$this->schema->create('', function (Blueprint $table) {
+			$table->increments('id');
+			$table->timestamps();
+		});
+
+        //Update table
+		$this->schema->table('', function (Blueprint $table) {
+			$table->increments('id');
+			$table->timestamps();
+		});
 	}
 
-	public function dow()
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+    public function down()
 	{
-		//
+		$this->schema->dropIfExists('');
 	}
 }
