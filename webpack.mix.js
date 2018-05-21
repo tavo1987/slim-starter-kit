e552a2e9e9faf31ffd20c73275e0a57311a73d25
tavo1987 <tavo198718@gmail.com>
2018-05-21 14:10:20 -0500

    Configure talwind css and purge css on webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,11 +1,13 @@
 let mix = require('laravel-mix');
+let tailwindcss = require('tailwindcss');
+require('laravel-mix-purgecss');
 
-mix.setPublicPath('./public');
-
-mix.sass('resources/assets/sass/app.scss', 'public/css/app.min.css')
+mix.setPublicPath('./public')
     .js('resources/assets/js/app.js', 'public/js/app.min.js')
+    .sass('resources/assets/sass/app.scss', 'public/css/app.min.css')
     .options({
         processCssUrls: false,
+        postCss: [ tailwindcss('./tailwind.js') ],
     })
     .browserSync({
         notify: false,
@@ -17,5 +19,10 @@ mix.sass('resources/assets/sass/app.scss', 'public/css/app.min.css')
         ],
         injectChanges: true,
     })
-
-
+    .purgeCss({
+        globs: [
+            path.join(__dirname, "./resources/views/**/*.twig"),
+            path.join(__dirname, "./resources/views/**/*.vue"),
+        ],
+        extensions: ['html', 'js', 'jsx', 'php', 'vue', 'twig'],
+    });
