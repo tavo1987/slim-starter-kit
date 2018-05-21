a86ddd83ef4c1969d9011cd44ed4afcdda14421d
tavo1987 <tavo198718@gmail.com>
2018-05-21 12:27:51 -0500

    Update route name to store leads

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,5 @@ use App\Controllers\LeadController;
 use App\Controllers\ThanksController;
 
 $app->get('/', HomeController::class.':index')->setName('home');
-$app->post('/store', LeadController::class.':store')->setName('store');
+$app->post('/store', LeadController::class.':store')->setName('leads.store');
 $app->get('/thanks', ThanksController::class.':index')->setName('thanks');
