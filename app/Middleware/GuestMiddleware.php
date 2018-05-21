f5e53332c91797d34a757ebbf2c3033436a2428c
tavo1987 <tavo198718@gmail.com>
2018-05-21 17:07:46 -0500

    Redirect to home route instead of reports on Guest middleware

--- app/Middleware/GuestMiddleware.php
+++ app/Middleware/GuestMiddleware.php
@@ -7,7 +7,7 @@ class GuestMiddleware extends Middleware
     public function __invoke($request, $response, $next)
     {
         if ($this->container->auth->check()) {
-            return $response->withRedirect($this->container->router->pathFor('reports'));
+            return $response->withRedirect($this->container->router->pathFor('home'));
         }
 
         $response = $next($request, $response);
